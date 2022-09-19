2261c45d44a0385fd8048eb3182fb3a556a56595
meihei3 <24579635+meihei3@users.noreply.github.com>
2022-09-19 12:21:50 +0900

    add scss compiler

---
+++ node_scripts/sass_compile.js
@@ -0,0 +1,13 @@
+const sass = require('sass');
+const path = require('path');
+const fs = require("fs");
+
+const src_path = path.join(__dirname, '../scss/style.scss');
+const dist_path = path.join(__dirname, '../css/dist.css');
+
+const result = sass.compile(src_path,  {style: "compressed"});
+
+fs.writeFile(dist_path, result.css, (err) => {
+    if (err) throw err;
+    console.log('正常に書き込みが完了しました');
+});
