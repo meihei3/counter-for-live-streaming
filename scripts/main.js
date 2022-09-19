1b67410e697d24baa085eca13eaa0ec3474fb537
meihei3 <24579635+meihei3@users.noreply.github.com>
2022-09-19 13:05:05 +0900

    add color picker

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,12 @@ const COUNTER_MIN = 0;
 const COUNTER_MAX = 100000;
 const zero_padding = Math.log10(COUNTER_MAX);
 
+const counter_display = document.getElementById("count-display");
 const counter_display_number = document.getElementById("count-number");
 const count_up_button = document.getElementById("count-up");
 const count_down_button = document.getElementById("count-down");
+const color_picker_bg = document.getElementById("bg-color-picker");
+const color_picker_text = document.getElementById("text-color-picker");
 
 const update_display = () => {
   counter_display_number.textContent = String(count).padStart(
@@ -26,6 +29,21 @@ const count_down = () => {
   update_display();
 };
 
+const bg_color_change = () => {
+    counter_display.style.backgroundColor = color_picker_bg.value;
+}
+
+const text_color_change = () => {
+    counter_display_number.style.color = color_picker_text.value;
+}
+
+const init = () => {
+    update_display();
+    bg_color_change();
+    text_color_change();
+}
+
 count_up_button.addEventListener("click", count_up);
 count_down_button.addEventListener("click", count_down);
-window.addEventListener("DOMContentLoaded", update_display);
+color_picker_bg.addEventListener("change", bg_color_change);
+window.addEventListener("DOMContentLoaded", init);
