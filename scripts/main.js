9134f40469e75539ba960b568682815e3daf6908
meihei3 <24579635+meihei3@users.noreply.github.com>
2022-09-19 12:42:08 +0900

    add function CountDown

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,31 @@
-(() => {
-    let count = 0;
+"use strict";
 
-    const counter_display_number = document.getElementById('count-number');
-    const count_up_button = document.getElementById('count-up');
+let count = 0;
+const COUNTER_MIN = 0;
+const COUNTER_MAX = 100000;
+const zero_padding = Math.log10(COUNTER_MAX);
 
-    const update_display = () => {
-        counter_display_number.textContent = String(count).padStart(5, '0');
-    };
+const counter_display_number = document.getElementById("count-number");
+const count_up_button = document.getElementById("count-up");
+const count_down_button = document.getElementById("count-down");
 
-    const count_up = () => {
-        count++;
-        update_display();
-    }
+const update_display = () => {
+  counter_display_number.textContent = String(count).padStart(
+    zero_padding,
+    "0"
+  );
+};
 
-    count_up_button.addEventListener('click', count_up)
-})();
+const count_up = () => {
+  count += count < COUNTER_MAX ? 1 : 0;
+  update_display();
+};
+
+const count_down = () => {
+  count -= count > COUNTER_MIN ? 1 : 0;
+  update_display();
+};
+
+count_up_button.addEventListener("click", count_up);
+count_down_button.addEventListener("click", count_down);
+window.addEventListener("DOMContentLoaded", update_display);
