a75d74af1c798d2a7b4e965442c0382a4d6e99d4
meihei3 <24579635+meihei3@users.noreply.github.com>
2022-11-10 22:57:26 +0900

    add about page

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,89 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div class="container">
+      <div class="header flex center">
+        <h1 class="header__text">
+          Counter For <br class="spbr" />
+          Live-streaming
+        </h1>
+      </div>
+      <div class="site-description background-ligth-gray">
+        <h2 class="site-description__title">About This</h2>
+        <p>
+          配信者向けのシンプルな手動カウンターWebアプリです。OBSでクロマキーフィルタを使用する事で透過させて使うことが出来ます。
+        </p>
+        <p>
+          ソースコードは
+          <a href="https://github.com/meihei3/counter-for-live-streaming">
+            GitHub
+          </a>
+          にて公開されています。
+        </p>
+        <h2 class="site-description__title">Privacy Policy</h2>
+        <p>
+          当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。
+          このGoogleアナリティクスはデータの収集のためにCookieを使用しています。
+          このデータは匿名で収集されており、個人を特定するものではありません。
+        </p>
+        <p>
+          この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。
+          この規約に関しての詳細は
+          <a href="https://marketingplatform.google.com/about/analytics/terms/jp/">
+            Googleアナリティクスサービス利用規約のページ
+          </a>
+          や
+          <a href="https://policies.google.com/technologies/ads?hl=ja">
+            Googleポリシーと規約
+          </a>
+          ページをご覧ください。
+        </p>
+      </div>
+      <div class="footer flex center">
+        <p>
+          <a href="/">top</a> | <a href="/about/">about</a> | Copyright © 2022{" "}
+          <a href="https://twitter.com/app1e_s">meihei</a>.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+
+export const Head = () => (
+  <>
+  <title>Counter for live-streaming</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1"/>
+  <meta charset="utf-8" />
+  <meta http-equiv="referrer-policy" content="strict-origin-when-cross-origin"/>
+  <link rel="shortcut icon" href={'/img/favicon.ico'} />
+  <link rel="stylesheet" href={'/css/wing.min.css'} />
+  <link rel="stylesheet" href={'/css/dist.css?v2.3.0'} />
+  <meta name="keywords" content="カウンター,配信,OBS,クロマキー,透過"/>
+  <meta name="description" content="配信者向けのシンプルな手動カウンターWebアプリです。OBSでクロマキーフィルタを使用する事で透過させて使うことが出来ます。"/>
+  <meta property="og:title" content="Counter for live-streaming"/>
+  <meta property="og:type" content="website"/>
+  <meta property="og:url" content="https://counter-app.meihei.dev/"/>
+  <meta property="og:image" content="https://counter-app.meihei.dev/static/img/ogp.png"/>
+  <meta property="og:site_name" content="Counter for live-streaming"/>
+  <meta property="og:description" content="配信者向けのシンプルな手動カウンターWebアプリです。OBSでクロマキーフィルタを使用する事で透過させて使うことが出来ます。"/>
+  <meta name="twitter:card" content="summary_large_image"/>
+  <meta name="twitter:site" content="@app1e_s"/>
+
+  {/* <!-- fonts --> */}
+  <link rel="preconnect" href="https://fonts.googleapis.com"/>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;300&family=Noto+Sans+JP:wght@300;400;700&display=swap&text=0123456789Counter For Live-streamig" rel="stylesheet"/>
+
+  {/* <!-- Google tag (gtag.js) --> */}
+  {/* <script nonce="NmLwe69hkSjN6cPvylnX_5zKwKkgZLzgy7CR0zAYR0c" async src="https://www.googletagmanager.com/gtag/js?id=G-NGEYCRYTBJ"></script>
+  <script nonce="NmLwe69hkSjN6cPvylnX_5zKwKkgZLzgy7CR0zAYR0c">
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+
+      gtag('config', 'G-NGEYCRYTBJ');
+  </script> */}
+  </>
+)
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,119 +1,116 @@
-"use strict";
+"use strict"
 
-let count = 0;
-const COUNTER_MIN = 0;
-const COUNTER_MAX = 100000;
-const zero_padding = Math.log10(COUNTER_MAX);
+let count = 0
+const COUNTER_MIN = 0
+const COUNTER_MAX = 100000
+const zero_padding = Math.log10(COUNTER_MAX)
 
-const counter_display = document.getElementById("count-display");
-const counter_display_number = document.getElementById("count-number");
-const count_up_button = document.getElementById("count-up");
-const count_down_button = document.getElementById("count-down");
-const color_picker_bg = document.getElementById("bg-color-picker");
-const color_picker_text = document.getElementById("text-color-picker");
-const use_border_checkbox = document.getElementById("use-border-checkbox");
-const font_selector = document.getElementById("font-selector");
+const counter_display = document.getElementById("count-display")
+const counter_display_number = document.getElementById("count-number")
+const count_up_button = document.getElementById("count-up")
+const count_down_button = document.getElementById("count-down")
+const color_picker_bg = document.getElementById("bg-color-picker")
+const color_picker_text = document.getElementById("text-color-picker")
+const use_border_checkbox = document.getElementById("use-border-checkbox")
+const font_selector = document.getElementById("font-selector")
 
 const update_display = () => {
-  counter_display_number.textContent = String(count).padStart(
-    zero_padding,
-    "0"
-  );
-};
+  counter_display_number.textContent = String(count).padStart(zero_padding, "0")
+}
 
 const count_up = () => {
-  count += count < COUNTER_MAX ? 1 : 0;
-  update_display();
-};
+  count += count < COUNTER_MAX ? 1 : 0
+  update_display()
+}
 
 const count_down = () => {
-  count -= count > COUNTER_MIN ? 1 : 0;
-  update_display();
-};
+  count -= count > COUNTER_MIN ? 1 : 0
+  update_display()
+}
 
 const bg_color_change = () => {
-  counter_display.style.backgroundColor = color_picker_bg.value;
-  localStorage.setItem("bg_color", color_picker_bg.value);
-};
+  counter_display.style.backgroundColor = color_picker_bg.value
+  localStorage.setItem("bg_color", color_picker_bg.value)
+}
 
 const text_color_change = () => {
-  counter_display_number.style.fill = color_picker_text.value;
-  localStorage.setItem("text_color", color_picker_text.value);
-};
+  counter_display_number.style.fill = color_picker_text.value
+  localStorage.setItem("text_color", color_picker_text.value)
+}
 
 const init_color = () => {
-  const b = localStorage.getItem("bg_color");
+  const b = localStorage.getItem("bg_color")
   if (b === null) {
-    counter_display.style.backgroundColor = color_picker_bg.value;
+    counter_display.style.backgroundColor = color_picker_bg.value
   } else {
-    counter_display.style.backgroundColor = b;
-    color_picker_bg.value = b;
+    counter_display.style.backgroundColor = b
+    color_picker_bg.value = b
   }
 
-  const t = localStorage.getItem("text_color");
+  const t = localStorage.getItem("text_color")
   if (t === null) {
-    counter_display_number.style.fill = color_picker_text.value;
+    counter_display_number.style.fill = color_picker_text.value
   } else {
-    counter_display_number.style.fill = t;
-    color_picker_text.value = t;
+    counter_display_number.style.fill = t
+    color_picker_text.value = t
   }
-};
+}
 
 const font_change = () => {
   switch (font_selector.value) {
     case "Noto Sans JP Bold":
-      counter_display_number.style.fontFamily = '"Noto Sans JP", sans-serif';
-      counter_display_number.style.fontWeight = 700;
-      localStorage.setItem("font", font_selector.value);
-      break;
+      counter_display_number.style.fontFamily = '"Noto Sans JP", sans-serif'
+      counter_display_number.style.fontWeight = 700
+      localStorage.setItem("font", font_selector.value)
+      break
     case "Noto Sans JP Regular":
-      counter_display_number.style.fontFamily = '"Noto Sans JP", sans-serif';
-      counter_display_number.style.fontWeight = 400;
-      localStorage.setItem("font", font_selector.value);
-      break;
+      counter_display_number.style.fontFamily = '"Noto Sans JP", sans-serif'
+      counter_display_number.style.fontWeight = 400
+      localStorage.setItem("font", font_selector.value)
+      break
     case "Noto Sans JP Ligth":
-      counter_display_number.style.fontFamily = '"Noto Sans JP", sans-serif';
-      counter_display_number.style.fontWeight = 300;
-      localStorage.setItem("font", font_selector.value);
-      break;
+      counter_display_number.style.fontFamily = '"Noto Sans JP", sans-serif'
+      counter_display_number.style.fontWeight = 300
+      localStorage.setItem("font", font_selector.value)
+      break
     default:
-      counter_display_number.style.fontFamily = "";
-      counter_display_number.style.fontWeight = "";
-      localStorage.removeItem("font");
-      break;
+      counter_display_number.style.fontFamily = ""
+      counter_display_number.style.fontWeight = ""
+      localStorage.removeItem("font")
+      break
   }
-};
+}
 
 const use_white_border = () => {
   if (use_border_checkbox.checked) {
-    if (!counter_display_number.classList.contains('number__stroke')) {
-      counter_display_number.classList.add('counter-number__stroke');
+    if (!counter_display_number.classList.contains("number__stroke")) {
+      counter_display_number.classList.add("counter-number__stroke")
     }
   } else {
-    if (counter_display_number.classList.contains('counter-number__stroke')) {
-      counter_display_number.classList.remove('counter-number__stroke');
+    if (counter_display_number.classList.contains("counter-number__stroke")) {
+      counter_display_number.classList.remove("counter-number__stroke")
     }
   }
 }
 
 const init_font = () => {
-  const f = localStorage.getItem("font");
+  const f = localStorage.getItem("font")
   if (f !== null) {
-    font_selector.value = f;
-    font_change();
+    font_selector.value = f
+    font_change()
   }
-};
+}
 
 const init = () => {
-  update_display();
-  init_color();
-  init_font();
-};
+  update_display()
+  init_color()
+  init_font()
+}
 
-count_up_button.addEventListener("click", count_up);
-count_down_button.addEventListener("click", count_down);
-color_picker_bg.addEventListener("change", bg_color_change);
-color_picker_text.addEventListener("change", text_color_change);
-font_selector.addEventListener("change", font_change);
-use_border_checkbox.addEventListener("change", use_white_border);
-window.addEventListener("DOMContentLoaded", init);
+count_up_button.addEventListener("click", count_up)
+count_down_button.addEventListener("click", count_down)
+color_picker_bg.addEventListener("change", bg_color_change)
+color_picker_text.addEventListener("change", text_color_change)
+font_selector.addEventListener("change", font_change)
+use_border_checkbox.addEventListener("change", use_white_border)
+window.addEventListener("DOMContentLoaded", init)
