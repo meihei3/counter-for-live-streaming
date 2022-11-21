67363328f58a1ac2d7376f42b7f86171085d2d2b
meihei3 <24579635+meihei3@users.noreply.github.com>
2022-11-22 02:17:50 +0900

    add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function NotFound() {
+  return (
+    <div class="container center flex">
+      <h1 class="not-found">404 Not Found</h1>
+    </div>
+  )
+}
+
+export { Head } from "../components/Head"
