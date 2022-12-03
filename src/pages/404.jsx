388f7682a30dff136449c55b87518236e11e18ce
meihei3 <24579635+meihei3@users.noreply.github.com>
2022-12-03 17:49:05 +0900

    update style css

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,13 +1,18 @@
 import React from "react"
+import styled from "styled-components"
 import { SEO } from "../components/Seo"
 
 export default function NotFound() {
   return (
     <div class="container center flex">
-      <h1 class="not-found">404 Not Found</h1>
+      <Title>404 Not Found</Title>
     </div>
   )
 }
+
+const Title = styled.h1`
+  font-weight: 200;
+`
 
 export const Head = () => (
   <SEO />
