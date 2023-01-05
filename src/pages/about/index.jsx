425efed0a600819709680798e2f6fdfaac8bb68d
meihei3 <24579635+meihei3@users.noreply.github.com>
2023-01-06 01:10:42 +0900

    update about page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -12,11 +12,21 @@ export default function About() {
           配信者向けのシンプルな手動カウンターWebアプリです。OBSでクロマキーフィルタを使用する事で透過させて使うことが出来ます。
         </p>
         <p>
-          ソースコードは
+          個人、法人、商用、非商用問わず無料でご利用頂けます。是非、配信でご活用ください。詳細は
+          <a href="/terms-of-service/">
+            利用規約
+          </a>
+          に記載しています。
+        </p>
+        <p>
+          安心してお使い頂けるように、全てのソースコードは
           <a href="https://github.com/meihei3/counter-for-live-streaming">
             GitHub
           </a>
           にて公開されています。
+        </p>
+        <p>
+        ご利用いただいた際には、本サービスを利用したことがわかるクレジット表記とリンクを記載して頂けると嬉しいです。
         </p>
       </Description>
     </Layout>
